7f0f5d14c75a9413e727a33abfba6e6b5139b984
Cédric <github@hidemyhome.ovh>
2019-11-03 18:54:33 +0100

    Suppression du mode watch sur webpack --> script d'install non stopé

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    watch:true,
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
